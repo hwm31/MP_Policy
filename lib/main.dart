5a695d7503840851863aab1ca4eca50334193abc
Hwa <132264214+hwm31@users.noreply.github.com>
2025-05-26 23:36:58 +0900

    main.dart firebase code modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'screens/home_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(MyApp());
 }
 
@@ -19,22 +12,10 @@ class MyApp extends StatelessWidget {
       title: '청년 일자리 커뮤니티',
       theme: ThemeData(
         primarySwatch: Colors.green,
+        fontFamily: 'NotoSansKR',
       ),
-      home: TestScreen(),
-    );
-  }
-}
-
-class TestScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase 테스트'),
-      ),
-      body: Center(
-        child: Text('Firebase 연결 성공!'),
-      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
